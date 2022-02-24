08b88c1f9290cd0ac396df67f734294eb4d5ff95
Liliya Lyubenova <lilly.lubenova@gmail.com>
2022-02-24 16:53:27 +0200

    [Add] User service interface that supports CRUD operations via Restful HTTP.

---
+++ app/src/main/java/com/diploma/project/vpts/service/UserService.java
@@ -0,0 +1,31 @@
+package com.diploma.project.vpts.service;
+
+import com.diploma.project.vpts.model.VPTSUser;
+
+import retrofit2.Call;
+import retrofit2.http.Body;
+import retrofit2.http.DELETE;
+import retrofit2.http.GET;
+import retrofit2.http.Header;
+import retrofit2.http.Headers;
+import retrofit2.http.POST;
+import retrofit2.http.PUT;
+import retrofit2.http.Path;
+
+public interface UserService {
+    @POST("user")
+    @Headers({"Content-Type: application/json"})
+    Call<VPTSUser> createUser(@Header("Authorization") String auth, @Body VPTSUser user);
+
+    @GET("user/{userId}")
+    @Headers({"Content-Type: application/json"})
+    Call<VPTSUser> getUser(@Header("Authorization") String auth, @Path("userId") String userId);
+
+    @PUT("user/{userId}")
+    @Headers({"Content-Type: application/json"})
+    Call<VPTSUser> updateUser(@Header("Authorization") String auth, @Path("userId") int id, @Body VPTSUser user);
+
+    @DELETE("user/{userId}")
+    @Headers({"Content-Type: application/json"})
+    Call<VPTSUser> deleteUser(@Header("Authorization") String auth, @Path("userId") String userId);
+}
