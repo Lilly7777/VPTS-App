b17fcc757a2c68453ac897542a519156afe20ce4
Liliya Lyubenova <lilly.lubenova@gmail.com>
2022-03-07 20:05:02 +0200

    [Add] Cache manager interface.

---
+++ app/src/main/java/com/diploma/project/vpts/service/AbstractCacheManager.java
@@ -0,0 +1,11 @@
+package com.diploma.project.vpts.service;
+
+import android.app.Activity;
+
+import com.diploma.project.vpts.model.VPTSUser;
+import com.fasterxml.jackson.core.JsonProcessingException;
+
+public interface AbstractCacheManager {
+    void saveUser(Activity activity, VPTSUser user) throws JsonProcessingException;
+    VPTSUser loadUser(Activity activity) throws JsonProcessingException, NullPointerException;
+}
