05297116d8b7286e971f6105ed39bf996d89fdac
Liliya Lyubenova <lilly.lubenova@gmail.com>
2022-03-23 15:38:48 +0200

    [Add] Added layouts and fragments for each button in the bottom navigation bar in the dashboard

---
+++ app/src/main/java/com/diploma/project/vpts/fragments/DevicesFragment.java
@@ -0,0 +1,24 @@
+package com.diploma.project.vpts.fragments;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.fragment.app.Fragment;
+
+import com.diploma.project.vpts.R;
+
+public class DevicesFragment extends Fragment {
+
+    public DevicesFragment() {
+        // Required empty public constructor
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_devices, container, false);
+    }
+}
